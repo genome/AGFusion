ad4962cc3003950a8fa859b552383e9e1831206b
ksinghal28 <kartiksinghal2814@gmail.com>
2022-10-20 22:34:12 -0500

    KS-fixing bug in previous commit

--- agfusion/parsers.py
+++ agfusion/parsers.py
@@ -27,7 +27,7 @@ class _Parser(object):
     def _check_data(self):
         if len(self.fusions) == 0:
             #self.logger.error("Read 0 fusions from the file! Exiting...")
-            if not os.path.exists(newpath):
+            if not os.path.exists("agfusion_results"):
                 os.mkdir("agfusion_results")
             with zipfile.ZipFile("/cromwell_root/agfusion_results.zip", mode="w") as archive:
                 archive.write("agfusion_results")
